5a38e5c89cf01978c2a4954d54b6bc71a80d28a8
Riza Fahmi <rizafahmi@gmail.com>
2024-09-01 21:24:02 +0700

    Deploy

--- lib/emoji_web/endpoint.ex
+++ lib/emoji_web/endpoint.ex
@@ -12,7 +12,7 @@ defmodule EmojiWeb.Endpoint do
   ]
 
   socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [session: @session_options]],
+    websocket: [connect_info: [session: @session_options], timeout: 45_000],
     longpoll: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
