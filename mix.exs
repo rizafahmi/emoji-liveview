99b98e3ce21387ecf933cea05e07d05728a28caa
Riza Fahmi <rizafahmi@gmail.com>
2024-09-04 13:56:06 +0700

    Change version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Emoji.MixProject do
     [
       app: :emoji,
       version: "0.1.0",
-      elixir: "~> 1.16.3",
+      elixir: "~> 1.17.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
